f3a0dfb81e5cf0e534d2a62a92f6c3a3a40807fa
Administrator <kristofer.souza2027@hotmail.com>
2023-08-17 17:52:56 -0800

    Foi ajustado a rolagem da tela para ser compatível com o tamanho do navbar de forma fixada

--- projeto_portfolio/app_portfolio/static/scripts/script.js
+++ projeto_portfolio/app_portfolio/static/scripts/script.js
@@ -1,16 +1,4 @@
-var links = document.querySelectorAll('.nav-bar a');
-
-// Função de rolagem suave
-function scrollSmoothHandler(e) {
-  e.preventDefault();
-  var target = document.querySelector(this.getAttribute('href'));
-  target.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  });
-}
-
-// Adiciona o evento de clique em cada link de navegação
-links.forEach(function(link) {
-  link.addEventListener('click', scrollSmoothHandler);
+// Inicializa o smooth-scroll com as configurações desejadas
+var scroll = new SmoothScroll('a[href*="#"]', {
+  offset: 90 // Seu ajuste de 90px para cima
 });
