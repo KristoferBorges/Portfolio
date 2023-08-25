cabe6ede8b71a3b702f0bd6592b695525e8551de
Administrator <kristofer.souza2027@hotmail.com>
2023-08-25 10:36:54 -0800

    Problema de carregamentos dos scripts resolvido, agora o botão de fazer download do CV funciona

--- projeto_portfolio/app_portfolio/static/scripts/download.js
+++ projeto_portfolio/app_portfolio/static/scripts/download.js
@@ -1,6 +1,8 @@
-document.getElementById('button-cv').addEventListener('click', function() {
-    var fileUrl = 'https://drive.google.com/file/d/1racF38dJgbW8eSoKCH3Z_e6c7xBHgxSB/view'; // Link do arquivo no Google Drive
-
-    // Abra o arquivo em uma nova aba
-    window.open(fileUrl, '_blank');
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('button-cv').addEventListener('click', function() {
+        var fileUrl = 'https://drive.google.com/file/d/1racF38dJgbW8eSoKCH3Z_e6c7xBHgxSB/view'; // Link do arquivo no Google Drive
+    
+        // Abra o arquivo em uma nova aba
+        window.open(fileUrl, '_blank');
+    });
 });
