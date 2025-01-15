3e27fd87091a1f16b4f59a54a1422e72629d74b6
Kristofer Borges <kristofer.souza2027@hotmail.com>
2025-01-15 11:50:31 -0300

    Ajustes antes de subir para o Railway 3#

--- projeto_portfolio/projeto_portfolio/settings.py
+++ projeto_portfolio/projeto_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m61il617vs+kbz(c*0g2qbsq#pa)((av_(+zw6t$8$63plecp0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['portfolio-kristofer-ffd1.up.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app_portfolio/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ projeto_portfolio/staticfiles/js/script.js
@@ -0,0 +1,4 @@
+// Inicializa o smooth-scroll com as configurações desejadas
+var scroll = new SmoothScroll('a[href*="#"]', {
+  offset: 90 // Seu ajuste de 90px para cima
+});
