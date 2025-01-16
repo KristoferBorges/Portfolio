c0d90e75ed2f80552470b4386046110cd36b01a1
Kristofer Borges <kristofer.souza2027@hotmail.com>
2025-01-15 22:13:50 -0300

    Implementação da trajetória, ajuste no currículo

--- projeto_portfolio/app_portfolio/static/js/script.js
+++
@@ -1,4 +0,0 @@
-// Inicializa o smooth-scroll com as configurações desejadas
-var scroll = new SmoothScroll('a[href*="#"]', {
-  offset: 90 // Seu ajuste de 90px para cima
-});
--- projeto_portfolio/staticfiles/js/download.js
+++ projeto_portfolio/staticfiles/js/download.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('button-cv').addEventListener('click', function() {
-        var fileUrl = 'https://docs.google.com/document/d/1uskvU2yrx_iMrwUj7-Lo8uzQ3UxlWcCB/edit?usp=sharing&ouid=101942351453446807090&rtpof=true&sd=true'; // Link do arquivo no Google Drive
+        var fileUrl = 'https://docs.google.com/document/d/13OdKePDpeJbgNxrp3pmLH207szHNnf-E/edit?usp=sharing&ouid=101942351453446807090&rtpof=true&sd=true'; // Link do arquivo no Google Drive
     
         // Abra o arquivo em uma nova aba
         window.open(fileUrl, '_blank');
