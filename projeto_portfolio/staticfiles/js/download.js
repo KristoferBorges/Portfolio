cf6d7d162736328e5d622b541d9fe8dddc33996f
Kristofer Borges <kristofer.souza2027@hotmail.com>
2025-07-19 12:25:44 -0300

    Atualização do curriculo

--- projeto_portfolio/staticfiles/js/download.js
+++ projeto_portfolio/staticfiles/js/download.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('button-cv').addEventListener('click', function() {
-        var fileUrl = 'https://docs.google.com/document/d/13OdKePDpeJbgNxrp3pmLH207szHNnf-E/edit?usp=sharing&ouid=101942351453446807090&rtpof=true&sd=true'; // Link do arquivo no Google Drive
+        var fileUrl = 'https://drive.google.com/file/d/1ehFr_wDa-HQD9grmfybzPi3h4QonNvC6/view?usp=drive_link'; // Link do arquivo no Google Drive
     
         // Abra o arquivo em uma nova aba
         window.open(fileUrl, '_blank');
